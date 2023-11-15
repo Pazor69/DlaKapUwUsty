ae05e0192c9f6e778d4e4a55bf1cf2b12a02fafe
dolezinski.jan <dolezinski.jan@STACJA105-02>
2023-11-15 14:11:48 +0100

    Dodaj pliki projektów.

---
+++ WpfApp1/Auta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+
+    enum Nadwozie
+    {
+        Coupe=0,
+        Sedan=1,
+        Combi=2,
+    }
+    internal class Auta
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+
+        public Nadwozie Nadwozie { get; set; } = 0;
+
+        public Auta(string marka, string model, Nadwozie nadwozie)
+        {
+            this.Nadwozie = nadwozie;
+            this.Marka = marka;
+            CheckTier();
+            Model = model;
+        }
+        public void CheckTier()
+        {
+            string jabol = Marka.ToLower();
+            string[] coupe = { "coupe"};
+            string[] sedan = { "sedan"};
+            string[] combi = { "combi"};
+             
+            
+                if (Marka.Equals(coupe[0]))
+                {
+                Nadwozie = Nadwozie.Coupe;
+                   
+                }
+                if (Marka.Equals(sedan[0]))
+                {
+                Nadwozie = Nadwozie.Sedan;
+                    
+                }
+                if (Marka.Equals(combi[0]))
+                {
+                Nadwozie = Nadwozie.Combi;
+                    
+                }
+            
+        }
+    }
+}
---
+++ WpfApp1/ListaAuta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class ListaAuta
+    {
+        public ObservableCollection<Auta> auta;
+        public ListaAuta() {
+            auta=new ObservableCollection<Auta>();
+
+        }
+        public void AddAuto(Auta auta)
+        {
+
+        }
+    }
+}
